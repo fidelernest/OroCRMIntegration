80c1a4db330fe6700e66e54f3fe22df18fa4d1c6
Ronel Pantaleon Agramonte <rpantaleon@opsecsecurity.com>
2022-04-09 00:59:25 -0400

    Lista de tareas

---
+++ OroClient/OroClient/Models/Tareas/Tarea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OroClient.Models.Tareas
+{
+    public class TasksRoot
+    {
+        public Tarea[] data { get; set; }
+    }
+    public class TaskRoot
+    {
+        public Tarea data { get; set; }
+    }
+
+    public class Tarea
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public taskAttributes attributes { get; set; }
+        public TaskRelationships relationships { get; set; }
+    }
+}
